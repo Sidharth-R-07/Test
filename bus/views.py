df73d4c85091554b70a9452f6fc5c49be107b541
shadow <sidhuram365@gmail.com>
2024-03-02 17:48:19 +0530

    added ksrtc link21

--- bus/views.py
+++ bus/views.py
@@ -1,21 +1,27 @@
 import requests
 
-def getData(request):
+def getData():
     # API endpoint
     print("getData CALLED-----------------------")
     api_url = "http://external.chalo.com/dashboard/enterprise/v1/vehicle/sessionData/thiruvananthapuram/ksrtc?vehicleId=KS3132"
-    external_auth='RWLXTEgMcmuMj1mehBWi3ROaAfTmQwXjGksxvxD9'
-    header={
-        "externalauth" : external_auth
+    external_auth = 'RWLXTEgMcmuMj1mehBWi3ROaAfTmQwXjGksxvxD9'
+    header = {
+        "externalauth": external_auth
     }
     
     try:
-        response = requests.get(api_url,headers = header,timeout=15)
+        response = requests.get(api_url, headers=header, timeout=15)
         if response.status_code == 200:
             data = response.json()
             print(data)
+            return data
         else:
             print("Error:", response.status_code)
+            return {"error": response.status_code}
     except Exception as e:
         print("An error occurred:", e)
+        return {"error": str(e)}
+
+
+
 
