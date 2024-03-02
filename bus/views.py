a661eddd09c7b85de3b4cf5e1013325936dea5d0
shadow <sidhuram365@gmail.com>
2024-03-02 18:50:28 +0530

    added ksrtc link

--- bus/views.py
+++ bus/views.py
@@ -4,13 +4,14 @@ def getData(request):
     # API endpoint
     print("------------GET DATA CALLED----------------")
     api_url = "https://external.chalo.com/dashboard/gtfs/realtime/thiruvananthapuram/ksrtc/bus"
+    external_auth = 'RWLXTEgMcmuMj1mehBWi3ROaAfTmQwXjGksxvxD9'
     headers = {
         "Content-Type": "application/json",
-        "externalauth":"RWLXTEgMcmuMj1mehBWi3ROaAfTmQwXjGksxvxD9"
+        "externalauth": external_auth
     }
     
     try:
-        response = requests.get(api_url)
+        response = requests.get(api_url,headers=headers)
         if response.status_code == 200:
             data = response.json()
             print(data)
