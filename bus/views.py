fa1abc79f195e7775b8b333e1d99de07ca4d6191
shadow <sidhuram365@gmail.com>
2024-03-02 17:09:14 +0530

    added ksrtc link

--- bus/views.py
+++ bus/views.py
@@ -2,10 +2,14 @@ import requests
 
 def getData(request):
     # API endpoint
-    api_url = "https://fakestoreapi.com/products"
+    api_url = "external.chalo.com/dashboard/gtfs/realtime/thiruvananthapuram/ksrtc/bus"
+    external_auth='RWLXTEgMcmuMj1mehBWi3ROaAfTmQwXjGksxvxD9'
+    header={
+        "externalauth" : external_auth
+    }
     
     try:
-        response = requests.get(api_url)
+        response = requests.get(api_url,headers = header)
         if response.status_code == 200:
             data = response.json()
             print(data)
