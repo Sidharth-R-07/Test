196f2cfe9fae079dec46b1f40e89addc618fce7d
shadow <sidhuram365@gmail.com>
2024-03-04 18:50:16 +0530

    .

--- bus/views.py
+++ bus/views.py
@@ -14,7 +14,7 @@ def getData(request):
         response = requests.get(api_url,headers=headers,timeout=10)
         print("STATUS CODE:"+str(response.status_code))        
         if response.status_code == 200:
-            print(response.text)
+            print(response.encoding)
             data = str(response.content)
             print(data)
             return JsonResponse(data, safe=False)
