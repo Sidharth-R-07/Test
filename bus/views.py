47a54079994e040936fe284e8c5ea06990bf8f79
shadow <sidhuram365@gmail.com>
2024-03-04 19:20:16 +0530

    .

--- bus/views.py
+++ bus/views.py
@@ -1,6 +1,3 @@
-import generated_pb2  # Import the generated Python code from your compiled Protobuf schema
-import json
-import base64
 import requests
 from django.http import JsonResponse
 
@@ -14,18 +11,12 @@ def getData(request):
     }
     
     try:
-        response = requests.get(api_url,headers=headers,timeout=10)
-        print("STATUS CODE:"+str(response.status_code))        
+        response = requests.get(api_url,headers=headers,timeout=10)        
         if response.status_code == 200:
-            binary_data = base64.b64decode(response.content)
-            protobuf_message = generated_pb2.YourMessage()
-            protobuf_message.ParseFromString(binary_data)
-            print("protobuf_message:::"+protobuf_message)
-            json_data = protobuf_json.pb2json(protobuf_message)
-            print(json_data)
-            data = str(response.content)
-            print(data)
-            return JsonResponse(data, safe=False)
+            decoded_data = response.content.decode('utf-8')  # Adjust the encoding if needed
+            print(decoded_data)
+            print("-----------DATA:::"+decoded_data)
+            return JsonResponse(response.content, safe=False)
         else:
             print("Error:", response.status_code)
             return JsonResponse({"error": response.status_code}, status=response.status_code)
