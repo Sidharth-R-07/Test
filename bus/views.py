5206986dfb95f8f702a9c834c39ceb482a71d79c
SIDHARTH R <105332264+Sidharth-R-07@users.noreply.github.com>
2024-03-05 17:07:09 +0530

    Update views.py

--- bus/views.py
+++ bus/views.py
@@ -1,32 +1,24 @@
 import json
 import requests
 import urllib3
-import base64
 from django.http import JsonResponse
+
 # Create a PoolManager instance
 http = urllib3.PoolManager()
-
 
 def getData(request):
     # API endpoint
     print("------------GET DATA CALLED----------------")
     api_url = "https://external.chalo.com/dashboard/gtfs/realtime/thiruvananthapuram/ksrtc/bus"
-    # externalauth = 'RWLXTEgMcmuMj1mehBWi3ROaAfTmQwXjGksxvxD9'
     headers = {
         "externalauth": 'RWLXTEgMcmuMj1mehBWi3ROaAfTmQwXjGksxvxD9'
     }
     try:
-        # response = requests.get(api_url,headers=headers,timeout=10)   
-        response = http.request('GET', api_url, headers=headers)  
-       
-        # print("DATA",str(response.content.decode('iso-8859-1')))
-        print("ROW DATA",response.data)
-        # print(type(response.raw))
-        return JsonResponse(response.data, safe=False)
-       
+        response = http.request('GET', api_url, headers=headers)
+        json_data = json.loads(response.data.decode('utf-8'))
+        print("JSON DATA", json_data)
+
+        return JsonResponse(json_data, safe=False)
     except Exception as e:
         print("An error occurred:", e)
         return JsonResponse({"error": str(e)}, status=500)
-
-
-
