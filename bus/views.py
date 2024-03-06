a2427e0185d91c595be76b1b81cff20af0b53b68
SIDHARTH R <105332264+Sidharth-R-07@users.noreply.github.com>
2024-03-06 12:41:10 +0530

    Update views.py

--- bus/views.py
+++ bus/views.py
@@ -19,7 +19,8 @@ def getData(request):
         feed = gtfs_realtime_pb2.FeedMessage()
         response = requests.get(api_url, headers=headers)
         data =  feed.ParseFromString(response.content)
-        print("Data", data)
+        print("Data", feed.entity)
+        type(feed.entity)
         return JsonResponse(data, safe=False)
      
     except Exception as e:
@@ -27,3 +28,25 @@ def getData(request):
         return JsonResponse({"error": str(e)}, status=500)
 
 
+def getBusDetailsFromId(request):
+    print("------------GET BUS DETAILS CALLED----------------")
+    # API endpoint
+    api_url = f"http://external.chalo.com/dashboard/enterprise/v1/vehicle/sessionData/thiruvananthapuram/ksrtc?vehicleId=KS3132"
+    externalauth = 'RWLXTEgMcmuMj1mehBWi3ROaAfTmQwXjGksxvxD9'
+    headers = {
+        "externalauth": externalauth
+    }
+    try:
+        response = requests.get(api_url, headers=headers)
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+            # Return the JSON data
+            return JsonResponse(data, safe=False)
+        else:
+            # Return an error response if the request was unsuccessful
+            return JsonResponse({"error": "Failed to fetch data"}, status=response.status_code)
+    except Exception as e:
+        print("An error occurred:", e)
+        return JsonResponse({"error": str(e)}, status=500)
